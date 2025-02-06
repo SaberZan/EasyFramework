d885b7cb2903f3e80826d3883f17536e50e1ba42
zhangshuaibo <zhangshuaibo@gg.com>
2025-02-06 17:13:32 +0800

    [修改] 修改UI框架

--- Client/Assets/Scripts/EasyFramework/Runtime/HotFix/UI/Components/UISafeAreaLayout/UISafeAreaLayout.cs
+++ Client/Assets/Scripts/EasyFramework/Runtime/HotFix/UI/Components/UISafeAreaLayout/UISafeAreaLayout.cs
@@ -1,22 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Easy
 {
     public class UISafeAreaLayout : MonoBehaviour
     {
+        private float curTime = 0f;
+        private ScreenOrientation preOrientation;
         private void Awake()
         {
+            CheckSafeArea();
+        }
+
+        private void Update()
+        {
+            curTime += Time.deltaTime;
+            if (curTime >= 0.2f)
+            {
+                CheckSafeArea();
+                curTime = 0;
+            }
+        }
+
+        private void CheckSafeArea()
+        {
+            if(preOrientation == Screen.orientation)
+            {
+                return;
+            }
+            preOrientation = Screen.orientation;
             RectTransform curTrans = transform.GetComponent<RectTransform>();
             if (curTrans != null)
             {
-                float safeArea = Screen.safeArea.yMin;
-                if ((Screen.height * 1.0f / Screen.width) > (16 / 8.9f))
+                if(Screen.orientation == ScreenOrientation.Portrait)
                 {
-                    safeArea = Screen.height * 1 / 28f;
+                    float safeArea = Screen.safeArea.yMin;
+                    if ((Screen.height * 1.0f / Screen.width) > (16 / 8.9f))
+                    {
+                        safeArea = Screen.height * 1 / 28f;
+                    }
+                    curTrans.offsetMin = Vector2.zero;
+                    curTrans.offsetMax = new Vector2(0.0f, -safeArea);
                 }
-                curTrans.offsetMax = new Vector2(0.0f, -safeArea);
+                else if(Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+                {
+                    float safeArea = Screen.safeArea.yMin;
+                    if ((Screen.height * 1.0f / Screen.width) > (16 / 8.9f))
+                    {
+                        safeArea = Screen.height * 1 / 28f;
+                    }
+                    curTrans.offsetMin = new Vector2(0.0f, safeArea);
+                    curTrans.offsetMax = Vector2.zero;
+                }
+                else if(Screen.orientation == ScreenOrientation.LandscapeLeft)
+                {
+                    float safeArea = Screen.safeArea.xMin;
+                    if ((Screen.width * 1.0f / Screen.height) > (16 / 8.9f))
+                    {
+                        safeArea = Screen.height * 1 / 28f;
+                    }
+                    curTrans.offsetMin = new Vector2(safeArea, 0);
+                    curTrans.offsetMax = Vector2.zero;
+                }
+                else if(Screen.orientation == ScreenOrientation.LandscapeRight)
+                {
+                    float safeArea = Screen.safeArea.xMin;
+                    if ((Screen.width * 1.0f / Screen.height) > (16 / 8.9f))
+                    {
+                        safeArea = Screen.height * 1 / 28f;
+                    }
+                    curTrans.offsetMin = Vector2.zero;
+                    curTrans.offsetMax = new Vector2(-safeArea, 0);
+                }
             }
         }
     }
